bf829e60df2c7596b5d03050c23f68c245eb5adf
Jesse Connell <ancon@upenn.edu>
2018-04-27 16:12:46 -0400

    initial commit

---
+++ wtf.R
@@ -0,0 +1,116 @@
+# The period character doesn't inherently have a special meaning, generally
+# speaking.
+x..y... <- 5
+
+# But ... is a keyword in R, like "if" or "for".
+?'...'
+
+# One other thing: S3 methods use periods in function names for special
+# purposes.
+plot.foo.bar <- function() {}
+getS3method("plot", "foo.bar") 
+
+# The base language is inconsistent between names_with_underscores,
+# names.with.periods, and camelCaseNames (or CamelCaseNames).  Examples:
+read.table
+UseMethod
+getS3method
+
+# functions return the value of the last statement implicitly.
+(function(){return(5)})() # 5
+(function(){5})() # 5
+# Just like for "if" or "for" the curly braces are optional for functions
+# consisting of a single statement.
+(function()5)() # 5
+
+# note that assignments themselves evaulate to the value assigned.
+x <- (y <- 5) # x is 5
+(function()x<-5)() # 5
+
+# we can assign rightwards too
+5 -> x
+# or even both ways at once!
+x <- 5 -> y # both are now 5
+
+# Here we set a default value.  Wait, how can y=A if A doesn't exist?  The
+# default assignment is lazy and won't actually happen until that variable is
+# accessed.  Compare that to Python where it happens at function definition
+# time.
+assign_when_used <- function(x, y=A) {
+	A <- x*2
+	print(y)
+}
+assign_when_used(5)
+
+# You can index things that look like scalars too; nearly everything is a
+# vector quantity in R.   (The only thing I've found that doesn't follow that
+# is a function.  Stored function calls, on the other hand, behave like lists,
+# and so even they are subsettable.)
+1[1] # 1
+1[2] # NA
+# Note that this can behave in weird ways for strings.
+length(c("string1", "string2")) # 2 strings, OK
+length(c("string1")) # just 1 string
+length("string1") # still just 1 string!
+# use nchar() for string length
+nchar("string1")
+# Note the implicit coercion happening with nchar.  the number of characters in
+# an NA object is two, FALSE is five, and TRUE is four!
+nchar(NA)
+nchar(F)
+nchar(T)
+# But NULL is zero-length, so you get a zero-length vector back. 
+nchar(NULL) # "integer(0)" is an empty vector of length values.
+nchar("") # "0" is not the same thing!
+
+# Operators are functions; references to them can be "escaped" with backticks
+# to refer to the thing in itself (as opposed to quote marks to refer to a
+# string
+# literal)
+print("+")
+print(`+`)
+#> print("+")
+#[1] "+"
+#> print(`+`)
+#function (e1, e2)  .Primitive("+")
+1 + 2
+(`+`)(1,2)
+
+# TRUE and FALSE are reserved keywords.  T and F are not!  They're just
+# variables pre-set to TRUE and FALSE that tempt you to rely on them because
+# they're just one character.  (Why, R?? Why do you do these things to us?)
+#
+#> ?`TRUE`
+# "TRUE and FALSE are reserved words denoting logical constants in the R
+# language, whereas T and F are global variables whose initial values set to
+# these"
+F <- TRUE # totally allowed
+if (F) { print("logical value was true") }
+F <- FALSE # back to sanity
+# Thanks Erik for pointing this out.
+
+# TODO: column name weirdness
+#> x <- data.frame(a=c(1,2,3), a=c(4,5,6), a=c(7,8,9), check.names=F)
+#> x
+#  a a a
+#1 1 4 7
+#2 2 5 8
+#3 3 6 9
+#> x$foo <- 3
+#> x
+#  a a a foo
+#1 1 4 7   3
+#2 2 5 8   3
+#3 3 6 9   3
+#> x[, 'bar'] <- 3
+#> x
+#  a a.1 a.2 foo bar
+#1 1   4   7   3   3
+#2 2   5   8   3   3
+#3 3   6   9   3   3
+
+# TODO: negative indexing gotchas with empty vectors (it'll remove all entries!)
+
+# TODO named/number indexing weirdness, like selecting row "3" versus 3.
+
+# TODO: copy-on-write behavior
